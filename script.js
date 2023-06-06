ce89f1642509e18d678bec79a61225595359f96e
IsidalCarino <isidalcarino@yahoo.com>
2023-06-06 21:42:26 +0400

    finale

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle ("sticky", window.scrollY > 100)
 });
+
+/*HAMBURGER*/
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx bx-menu');
+    navlist.classList.toggle('open');
+}
